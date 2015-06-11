9d385d3dda597375eac329a02a2573dd8ec8be3e
Uice Lu <uicestone@gmail.com>
2015-06-11 10:01:59 +0800

    投顾访问产品的编辑页面，其他人访问查看页面
    修复: 产品净值无法打开

--- app/views/consultant/list.php
+++ app/views/consultant/list.php
@@ -12,7 +12,7 @@
 			<h4><?=$consultant->name?> <span class="small"><?=$consultant->type?></span></h4>
 		</td>
 		<td>
-			<a href="<?=url('consultant/' . $consultant->id)?>" class="btn btn-default"><span class="glyphicon glyphicon-edit"></span> 资料</a>
+			<a href="<?=isset($user) && $user->is_admin ? url('consultant/' . $consultant->id . '/edit') : url('consultant/' . $consultant->id)?>" class="btn btn-default"><span class="glyphicon glyphicon-edit"></span> 资料</a>
 			<a href="<?=url()?>" class="btn btn-default"><span class="glyphicon glyphicon-paperclip"></span> 动态</a>
 		</td>
 		<td>
